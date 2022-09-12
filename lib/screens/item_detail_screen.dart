df461f2932c72f85ce8309f286500e5a1a551db9
kasempan <kasempantananjai@gmail.com>
2022-09-12 17:34:28 +0700

    item_detail

--- lib/screens/item_detail_screen.dart
+++ lib/screens/item_detail_screen.dart
@@ -23,12 +23,86 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('รายละเอียด'),
+        centerTitle: true,
       ),
-      body: Hero(
-        tag: 'heroItem${args.itemIndex}',
-        child: Image.network(
-          item.imagesUrl[0],
-          fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Hero(
+                tag: 'heroItem${args.itemIndex}',
+                child: Image.network(
+                  item.imagesUrl[0],
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 45,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 8,
+                    child: Container(
+                      padding: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        boxShadow: [],
+                      ),
+                      child: Text(
+                        item.name,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: item.itemType == 'แลกเปลื่ยน'
+                            ? Theme.of(context).primaryColorLight
+                            : Color(0XFF68EDD2),
+                        boxShadow: [],
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        item.itemType == 'แลกเปลื่ยน' ? 'แลก' : 'ให้',
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(10),
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: Text(
+                item.detail,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: GestureDetector(
+        child: BottomAppBar(
+          color: item.itemType == "แลกเปลื่ยน" ? Theme.of(context).primaryColor : Color(0XFF68EDD2),
+          child: Container(
+            height: 40,
+            child: Text(
+              item.itemType == "แลกเปลื่ยน" ? 'เสนอ' : 'ขอ',
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ),
     );
